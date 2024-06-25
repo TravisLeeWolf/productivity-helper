7ce297a36fc7b1173fa69ac9b607bfa8c9414970
TravisLeeWolf <42667524+TravisLeeWolf@users.noreply.github.com>
2024-06-25 23:14:45 +0900

    add assets with v0.1 working

--- godot/Main.gd
+++ godot/Main.gd
@@ -7,11 +7,33 @@ var today
 
 func _ready() -> void:
 	today = Time.get_date_dict_from_system()
-	get_minute()
+	print(today)
+	update_weekday_view(get_week())
+	update_minute_view(get_minute())
+	update_day_view(get_day())
+	
 
-func get_minute() -> String:
+func get_minute() -> int:
 	var time = Time.get_time_string_from_system().split(":")
-	return time[1]
+	return int(time[1])
+	
+func update_minute_view(minute) -> void:
+	$Stack/Minute.value = minute
 
 func _on_Timer_timeout() -> void:
-	print(get_minute())
+	update_minute_view(get_minute())
+	
+func get_week() -> int:
+	return today.weekday
+	
+func update_weekday_view(week) -> void:
+	if week == 0:
+		$Stack/Week.value = 7
+	else:
+		$Stack/Week.value = week
+		
+func get_day() -> int:
+	return today.day
+	
+func update_day_view(day) -> void:
+	$Stack/Day.value = day
