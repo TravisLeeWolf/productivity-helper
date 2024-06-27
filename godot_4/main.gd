c44ccb866ef18c8d40c1806306064c3f3635052e
TravisLeeWolf <42667524+TravisLeeWolf@users.noreply.github.com>
2024-06-28 00:09:22 +0900

    minor function updates

--- godot_4/main.gd
+++ godot_4/main.gd
@@ -5,8 +5,7 @@ var today
 func _ready():
 	today = get_date()
 	update_time()
-	update_week()
-	update_month()
+	update_date()
 	
 func get_date() -> Dictionary:
 	return Time.get_date_dict_from_system()
@@ -29,6 +28,10 @@ func update_month() -> void:
 	$Grid/MonthBar.value = day
 	$Grid/Days.text = str(day)
 	
+func update_date() -> void:
+	update_week()
+	update_month()
+	
 func get_time() -> Dictionary:
 	return Time.get_time_dict_from_system()
 	
@@ -37,8 +40,12 @@ func update_time() -> void:
 	var hour = get_time().hour
 	if hour > 12:
 		hour = hour - 12
-	$Grid/MinuteBar.value = minute
-	$Grid/MinuteBar/HourText.text = str(hour)
+	elif hour == 0:
+		$Grid/MinuteBar/HourText.text = "12"
+		update_date()
+	else:
+		$Grid/MinuteBar.value = minute
+		$Grid/MinuteBar/HourText.text = str(hour)
 
 func _on_timer_timeout():
 	update_time()
